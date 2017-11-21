2ae53f4d0c0a785c9281da4c9a19768d7fc90759
Sezny Watkins <swatkins@clyde>
2017-11-20 22:06:55 -0500

    couple more functions, tested

--- ai.h
+++ ai.h
@@ -31,7 +31,39 @@ int home(int *pits){
 }
 
 //checks if the opponent can land in an empty pit & steal the AI's marbles
+//returns the number of the biggest pit they could steal from
+//or -1 if they can't steal anything
 int defense (int *theirs, int *mine){
+    int concern=-1;
+    //loop through their pits to find any that are empty
+    //&& across from any of yours that aren't
+    for (int i=0; i<6;i++){
+	//if so, see if they can end in it
+	if (theirs[i]==0 && mine[5-i]>0 && mine[5-i]>mine[concern]){
+	    //boolean for if they can end there or not
+	    int worry=0;
+	    for(int j=i+1; j<6; j++){
+		if (theirs[j]==(5-j)+7+(i+1)){
+		    worry=1;
+		    break;
+		}
+	    }
+	    for (int j=0; j<i; j++){
+		if (worry==1){
+		    break;
+		}
+		if (theirs[j]==i-j || theirs[j]==(i-j)+13){
+		    worry=1;
+		    break;
+		}
+	    }
+	    //if so, that's our new concern
+	    if (worry==1){
+		concern=5-i; 
+	    }
+	}
+    }
+    return concern;
 }
 
 #endif
