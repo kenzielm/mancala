cc8bba02267bfd3e2d79ecd770319ac4526a126f
Sezny Watkins <swatkins@clyde>
2017-11-22 15:57:21 -0500

    added medium

--- levels.h
+++ levels.h
@@ -18,7 +18,32 @@ int easy(int*pits){
 }
 
 
-int medium(int* AIpits, int *PlayerPits, int AIscore, int PlayerScore){
+int medium(int* AIpits, int *PlayerPits){
+    //if we can end in the home bucket and get another turn, do it
+    int tryHome=home(AIpits);
+    if (tryHome!=-1){
+	return tryHome;
+    }
+
+    //otherwise, try to land in an empty pit & steal some of theirs
+    int tryTheivery=theivery(PlayerPits, AIpits);
+    if (tryTheivery!=-1){
+	return tryTheivery;
+    }
+
+    //otherwise check if they can steal some of yours
+    int tryDefense=defense(PlayerPits, AIpits);
+    if (tryDefense!=-1){
+	return tryDefense;
+    }
+
+    //if none of those work this AI thinks there's nothing strategic to do
+    //and just makes a random move
+    int move=random();
+    while(AIpits[move]==0){
+	move=random();
+    }
+    return move;
 }
 
 int hard(int* AIpits, int *PlayerPits, int AIscore, int PlayerScore){
