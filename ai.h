fc1a865b1f1beac8fdf7989e235eea77f7dde00b
Sezny Watkins <swatkins@clyde>
2017-11-22 15:45:39 -0500

    all the functions, tested

--- ai.h
+++ ai.h
@@ -30,6 +30,35 @@ int home(int *pits){
     return -1;
 }
 
+//see if you can steal any of theirs
+//if not return -1
+//if so, return which pit you need to move
+//return the one that can steal the most if there are multiples
+int theivery(int *theirs, int *mine){
+    //what to move
+    int move=-1;
+    //how many we're stealing (for determining the best one)
+    int taking=0;
+    //check each of your pits
+    for (int i=0; i<6; i++){
+	if (mine[i]>0){
+	    //where it ends
+	    int land=(i+mine[i])%13;
+	    //check if it will be empty
+	    if (land<6 && (land==i || mine[land]==0)){
+		 //can we take more than the old one?
+		if(theirs[5-land]>taking){
+		    //if so, that's our new move - update our variables
+		    taking=theirs[5-land];
+		    move=i;
+		}
+	    }
+	}
+    }
+    return move;
+}
+
+
 //checks if the opponent can land in an empty pit & steal the AI's marbles
 //returns the number of the biggest pit they could steal from
 //or -1 if they can't steal anything
@@ -65,5 +94,57 @@ int defense (int *theirs, int *mine){
     }
     return concern;
 }
+
+
+//determines if you're about to run out of marbles & lose
+//returns 1 if we're about to lose, 0 otherwise
+int losing(int score, int *pits){
+    //if we've got more than half in our home, we're fine
+    if (score>24){
+	return 0;
+    }
+    //otherwise, check how many pits we still occupy
+    int empty=0;
+    for (int i=0; i<6; i++){
+	//if we're in more than 3 we're fine 
+	if (empty>3){
+	    return 0;
+	}
+	if (pits[i]>0){
+	    empty++;
+	}
+    }
+    if (empty>3){
+	return 0;
+    }
+    else{
+	return 1;
+    }
+}
+
+//finds the best one to move to be able to land there next time
+int thinkAhead(int *theirs, int *mine){
+    //the move
+    int move=-1;
+    //how many marbles we might be able to steal next turn from the current move
+    int steal=0;
+    //loop through your pits to find the best one to move
+    for (int i=0; i<6; i++){
+	//can we move & could it be better than our current move
+	if(mine[i]>0 && theirs[5-i]>steal){
+	    //loop through to see if moving any others next turn could end in this bucket
+	    //account for the fact that there might be an additional marbel in the pits next turn
+	    for(int j=0; j<6; j++){
+		if((j+mine[j])%13==i || (j+1+mine[j])%13==i){
+		    move=i;
+		    steal=theirs[5-j];
+		    break;
+		}
+	    }
+	}
+    }
+    return move;
+}
+
 
 #endif
